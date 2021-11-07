ea08f93ac17814864e7038ce4f71c9b71aad5089
Domenico Leuzzi <domenico.leuzzi@studenti.unicz.it>
2021-11-07 04:53:31 +0100

    Fix Connection Modules

--- ArduinoPedalBoard/ArduinoPedalBoard.py
+++ ArduinoPedalBoard/ArduinoPedalBoard.py
@@ -14,145 +14,6 @@ except ModuleNotFoundError:
   import serial
   import serial.tools.list_ports
 
-#
-# ArduinoAppTemplate
-#
-
-class ArduinoAppTemplate():
-  """ Template class for writing code on top of Arduino Connector
-  """
-  def __init__(self):
-
-    self.ArduinoNode = slicer.mrmlScene.GetFirstNodeByName("arduinoNode")
-    sceneModifiedObserverTag = self.ArduinoNode.AddObserver(vtk.vtkCommand.ModifiedEvent, self.doSomethingWhenNewDataIsRead)
-
-  def doSomethingWhenNewDataIsRead(self, caller, event):
-    #EXAMPLE TO PRINT THE RECEIVED VALUE:
-    print("FIRED! %s" % (self.ArduinoNode.GetParameter("Data")))
-
-  def sendDataToArduino(self, message):
-    messageSent = slicer.modules.arduinoconnect.widgetRepresentation().self().logic.sendMessage(message)
-
-#
-#ArduinoPlotter
-#
-
-class ArduinoPlotter():
-  def __init__(self, numberOfSamples):
-
-    self.active = True
-
-    self.ArduinoNode = slicer.mrmlScene.GetFirstNodeByName("arduinoNode")
-    sceneModifiedObserverTag = self.ArduinoNode.AddObserver(vtk.vtkCommand.ModifiedEvent, self.addPointToPlot)
-
-    # Add data into table vtk
-    self.tableNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLTableNode")
-    self.tableNode.SetName("Arduino plotting table")
-    self.table = self.tableNode.GetTable()
-
-    self.numberOfSamples = numberOfSamples
-    self.initializeTable()
-
-    # Create plot node
-    self.plotSeriesNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLPlotSeriesNode", "Amplitude")
-    self.plotSeriesNode.SetName("Arduino plot series")
-    self.plotSeriesNode.SetAndObserveTableNodeID(self.tableNode.GetID())
-    self.plotSeriesNode.SetXColumnName("Samples")
-    self.plotSeriesNode.SetYColumnName("Amplitude")
-    self.plotSeriesNode.SetPlotType(slicer.vtkMRMLPlotSeriesNode.PlotTypeLine)
-    self.plotSeriesNode.SetLineStyle(slicer.vtkMRMLPlotSeriesNode.LineStyleSolid)
-    self.plotSeriesNode.SetMarkerStyle(slicer.vtkMRMLPlotSeriesNode.MarkerStyleSquare)
-    self.plotSeriesNode.SetUniqueColor()
-
-    # Create plot chart node
-    self.plotChartNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLPlotChartNode")
-    self.plotChartNode.SetName("Arduino plot chart")
-    self.plotChartNode.AddAndObservePlotSeriesNodeID(self.plotSeriesNode.GetID())
-    self.plotChartNode.SetTitle('Arduino Data')
-    self.plotChartNode.SetXAxisTitle('Samples')
-    self.plotChartNode.SetYAxisTitle('Amplitude')
-    self.plotChartNode.LegendVisibilityOff()
-    self.plotChartNode.SetXAxisRangeAuto(True)
-    self.plotChartNode.SetYAxisRangeAuto(True)
-
-    # Switch to a layout that contains a plot view to create a plot widget
-    self.layoutManager = slicer.app.layoutManager()
-    layoutWithPlot = slicer.modules.plots.logic().GetLayoutWithPlot(self.layoutManager.layout)
-    self.layoutManager.setLayout(layoutWithPlot)
-
-    # Select chart in plot view
-    self.plotWidget = self.layoutManager.plotWidget(0)
-    self.plotViewNode = self.plotWidget.mrmlPlotViewNode()
-    self.plotViewNode.SetPlotChartNodeID(self.plotChartNode.GetID())
-
-  def initializeTable(self):
-
-    self.table.Initialize()
-
-    self.arrX = vtk.vtkFloatArray()
-    self.arrX.SetName("Samples")
-    self.table.AddColumn(self.arrX)
-
-    self.arrY = vtk.vtkFloatArray()
-    self.arrY.SetName("Amplitude")
-    self.table.AddColumn(self.arrY)
-
-    self.table.SetNumberOfRows(self.numberOfSamples)
-    for i in range(self.numberOfSamples):
-        self.table.SetValue(i, 0, i)
-        self.table.SetValue(i, 1, 0)
-
-    self.table.Modified()
-
-  def addPointToPlot(self, caller, event):
-
-    if self.active:
-
-      # Only float data type can be plot
-      try:
-        messageFloat = float(self.ArduinoNode.GetParameter("Data"))
-      except ValueError:
-        return
-
-      self.arrY.InsertNextTuple1(messageFloat)
-      self.arrY.RemoveFirstTuple()
-
-      self.table.Modified()
-      self.plotWidget.plotView().fitToContent()
-
-#
-# Arduino Monitor
-#
-
-class ArduinoMonitor(ScriptedLoadableModule):
-  """ Class for plotting arduno data into a separate window
-  """
-  def __init__(self):
-
-    self.ArduinoNode = slicer.mrmlScene.GetFirstNodeByName("arduinoNode")
-    sceneModifiedObserverTag = self.ArduinoNode.AddObserver(vtk.vtkCommand.ModifiedEvent, self.addLine)
-
-    self.monitor = qt.QTextEdit()
-    self.monitor.setWindowTitle("Arduino monitor")
-    self.monitor.setReadOnly(True)
-    self.monitor.show()
-
-    self.messageLenghtLimit = 50
-
-  def addLine(self, caller, event):
-    message = self.ArduinoNode.GetParameter("Data")
-
-    if len(message) > self.messageLenghtLimit:
-      message = "WARNING: message too long to be shown here\n"
-    elif len(message) <= self.messageLenghtLimit and not message.endswith("\n"):
-      message = message + "\n"
-
-    self.monitor.insertPlainText(message)
-
-    # Show always the last message
-    verticalScrollBar = self.monitor.verticalScrollBar()
-    verticalScrollBar.setValue(verticalScrollBar.maximum)
-    
 
 #
 # ArduinoPedalBoard Class dev. Domenico Leuzzi
@@ -160,23 +21,12 @@ class ArduinoMonitor(ScriptedLoadableModule):
 
 
 class ArduinoPedalBoardViews(ScriptedLoadableModule):
-
-
-  def __init__(self,parent):
-    ScriptedLoadableModule.__init__(self, parent)
-    self.parent.title = "ArduinoPedalBoard" # TODO make this more human readable by adding spaces
-    self.parent.categories = ["Developer Tools"]
-    self.parent.dependencies = []
-    self.parent.contributors = ["Paolo Zaffino (Magna Graecia University of Catanzaro, Italy)", "Domenico Leuzzi (Magna Graecia University of Catanzaro, Italy)", "Virgilio Sabatino (Magna Graecia University of Catanzaro, Italy)", "Andras Lasso (PerkLab, Queen's)", "Maria Francesca Spadea (Magna Graecia University of Catanzaro, Italy)"]
-    self.parent.helpText = """
-    This module allows to connect and transmit/receive data from Arduino board. On top of this users can build applications.
-"""
-    self.parent.helpText += self.getDefaultModuleDocumentationLink()
-    self.parent.acknowledgementText = """ """ # replace with organization, grant and thanks.
     
+    
+  def __init__(self):
     
     self.ArduinoNode = slicer.mrmlScene.GetFirstNodeByName("arduinoNode")
-    #sceneModifiedObserverTag = self.ArduinoNode.AddObserver(vtk.vtkCommand.ModifiedEvent, self.button3IsPushed)  #QUESTO CI DEVE ESSERE VEDERE DOVE METTERLO
+    sceneModifiedObserverTag = self.ArduinoNode.AddObserver(vtk.vtkCommand.ModifiedEvent, self.button3IsPushed)
     
     # Get Slice Node from Scene
     self.red_Slice = slicer.mrmlScene.GetNodeByID("vtkMRMLSliceNodeRed")
@@ -563,6 +413,7 @@ class ArduinoPedalBoard(ScriptedLoadableModule):
     self.parent.helpText += self.getDefaultModuleDocumentationLink()
     self.parent.acknowledgementText = """ """ # replace with organization, grant and thanks. 
 
+
 #
 # ArduinoConnectWidget
 #
@@ -589,22 +440,6 @@ class ArduinoPedalBoardWidget(ScriptedLoadableModuleWidget):
     uiWidget = slicer.util.loadUI(self.resourcePath('UI/ArduinoPedalBoard.ui'))
     self.layout.addWidget(uiWidget)
     self.ui = slicer.util.childWidgetVariables(uiWidget)
-
-    # Set IDE labels
-    self.arduinoIDEExe = self.config["IDEExe"]
-    if self.arduinoIDEExe == "":
-      self.arduinoIDEExe = self.autoFindIDEExe()
-    self.ui.IDEPathText.setText(self.arduinoIDEExe)
-
-    # connections
-    self.ui.portSelectorComboBox.setEnabled(False)
-    self.ui.detectDevice.connect('clicked(bool)', self.onDetectDeviceButton)
-    self.ui.connectButton.connect('toggled(bool)', self.onConnectButton)
-    self.ui.setIDEButton.connect('clicked(bool)', self.onSetIDEButton)
-    self.ui.runIDEButton.connect('clicked(bool)', self.onRunIDEButton)
-    self.ui.monitorButton.connect('clicked(bool)', self.onMonitorButton)
-    self.ui.plotterButton.connect('toggled(bool)', self.onPlotterButton)
-    self.ui.samplesToPlotText.textChanged.connect(self.onSamplesToPlot)  
     
     #Edit By Domenico Leuzzi
     #(NEW BUTTONS)
@@ -614,10 +449,7 @@ class ArduinoPedalBoardWidget(ScriptedLoadableModuleWidget):
 
     # Add vertical spacer
     self.layout.addStretch(1)
-
-    # Default values for QLineEdit
-    self.ui.samplesPerSecondText.setText("10")
-    self.ui.samplesToPlotText.setText("30")
+    
 
   def cleanup(self):
     pass
@@ -632,61 +464,9 @@ class ArduinoPedalBoardWidget(ScriptedLoadableModuleWidget):
       return ""
     else:
       return arduinoIDEExe
-
-  def onConnectButton(self, toggle):
-  
-  
-    # clicked connect and the device list has elements
-    if toggle and self.ui.portSelectorComboBox.currentText != "":
-
-        self.connected = self.logic.connect(self.ui.portSelectorComboBox.currentText,
-                                            self.ui.baudSelectorComboBox.currentText,
-                                            self.ui.samplesPerSecondText.text)
-        
-        if self.connected:
-          self.ui.connectButton.setText("Disconnect")
-          self.ui.connectButton.setStyleSheet("background-color:#ff0000")
-          self.ui.portSelectorComboBox.setEnabled(False)
-          self.ui.baudSelectorComboBox.setEnabled(False)
-          self.ui.detectDevice.setEnabled(False)
-          self.ui.samplesPerSecondText.setEnabled(False)
-
-        else:
-          self.deviceError("Device not found", "Impssible to connect the selected device.", "critical")
-          self.ui.connectButton.setChecked(False)
-          self.ui.connectButton.setText("Connect")
-          self.ui.connectButton.setStyleSheet("background-color:#f1f1f1;")
-
-    # clicked connect but device list has no elements
-    elif toggle and self.ui.portSelectorComboBox.currentText == "":
-        self.deviceError("Ports scan", "Any device has been set!", "warning")
-        self.ui.connectButton.setChecked(False)
-        return
-
-    # clicked disconnect with a running connection
-    elif not toggle and self.logic.arduinoConnection is not None and self.connected:
-      self.logic.disconnect()
-      self.ui.connectButton.setText("Connect")
-      self.ui.connectButton.setStyleSheet("background-color:#f1f1f1;")
-      self.ui.portSelectorComboBox.setEnabled(True)
-      self.ui.baudSelectorComboBox.setEnabled(True)
-      self.ui.detectDevice.setEnabled(True)
-      self.ui.samplesPerSecondText.setEnabled(True)
-
-  def onDetectDeviceButton(self, clicked):
-
-    self.ui.portSelectorComboBox.setEnabled(True)
-    self.ui.portSelectorComboBox.clear()
-
-    devices = [port.device for port in serial.tools.list_ports.comports() if port[2] != 'n/a']
-
-    if len(devices)==0:
-        self.deviceError("Ports scan", "Any device has been found!", "warning")
-    elif len(devices)>0:
-        for device in devices:
-            self.ui.portSelectorComboBox.addItem(device)
-            
-       
+      
+      
+      
   def onSetButton1(self, clicked): 
 
     # Alert
@@ -768,46 +548,8 @@ class ArduinoPedalBoardWidget(ScriptedLoadableModuleWidget):
     elif((self.ui.button1_Choice.currentText=="Select Option") or (self.ui.button2_Choice.currentText=="Select Option") or (self.ui.button3_Choice.currentText=="Select Option")):
     
         self.deviceError("Missed Option", "Changer Viewer not found!", "warning")
-
-  def onSetIDEButton(self, clicked):
-    dialog = qt.QFileDialog()
-    self.arduinoIDEExe = dialog.getOpenFileName(None, "Arduino IDE executable", os.path.expanduser("~"))
-    self.ui.IDEPathText.setText(self.arduinoIDEExe)
-
-    # Update config
-    self.config["IDEExe"] = self.arduinoIDEExe
-    self.writeConfig()
-
-  def onRunIDEButton(self, clicked):
-    if self.arduinoIDEExe != "":
-      subprocess.Popen(self.arduinoIDEExe)
-
-  def onSendButton(self, clicked):
-    message = self.ui.messageText.text
-    self.logic.sendMessage(message)
-
-  def onMonitorButton(self, clicked):
-    monitor = ArduinoMonitor()
-
-  def onPlotterButton(self, clicked):
-    if clicked and self.plotter is None:
-      self.plotter = ArduinoPlotter(int(self.ui.samplesToPlotText.text))
-      self.ui.plotterButton.setText("Stop plotting")
-
-    if not clicked and self.plotter is not None:
-      self.plotter.active = False
-      self.ui.plotterButton.setText("Plot data")
-
-    if clicked and self.plotter is not None:
-      self.plotter.active = True
-      self.ui.plotterButton.setText("Stop plotting")
-
-  def onSamplesToPlot(self, event):
-    samplesToPlot = int(self.ui.samplesToPlotText.text)
-    if self.plotter is not None and samplesToPlot > 0:
-      self.plotter.numberOfSamples = samplesToPlot
-      self.plotter.initializeTable()
-
+             
+        
   def deviceError(self, title, message, error_type="warning"):
     deviceMBox = qt.QMessageBox()
     if error_type == "warning":
@@ -841,56 +583,6 @@ class ArduinoPedalBoardLogic(ScriptedLoadableModuleLogic):
       self.parameterNode.SetName("arduinoNode")
       slicer.mrmlScene.AddNode(self.parameterNode)
 
-      self.arduinoConnection = None
-
-  def sendMessage(self, messageToSend):
-      #print(messageToSend)
-      if self.arduinoConnection is not None:
-        self.arduinoConnection.write(str.encode(messageToSend))
-        return True
-      else:
-        return False
-
-  def connect(self, port, baud, samplesPerSecond):
-      self.arduinoEndOfLine = '\n'
-      self.arduinoRefreshRateFps = float(samplesPerSecond)
-
-      try:
-        self.arduinoConnection = serial.Serial(port, baud)
-      except serial.serialutil.SerialException:
-        return False
-
-      qt.QTimer.singleShot(1000/self.arduinoRefreshRateFps, self.pollSerialDevice)
-      return True
-
-  def disconnect(self):
-      self.arduinoConnection.close()
-      self.arduinoConnection = None
-
-  def pollSerialDevice(self):
-
-      if self.arduinoConnection is None:
-        return
-
-      if self.arduinoConnection.isOpen() and self.arduinoConnection.in_waiting == 0: # No messages from arduino
-          qt.QTimer.singleShot(1000/self.arduinoRefreshRateFps, self.pollSerialDevice)
-      elif self.arduinoConnection.isOpen() and self.arduinoConnection.in_waiting > 0: # Some messages from arduino
-          arduinoReceiveBuffer = self.arduinoConnection.readline().decode('ascii')
-          if self.arduinoEndOfLine in arduinoReceiveBuffer: # Valid message
-              message = arduinoReceiveBuffer.split(self.arduinoEndOfLine)[0]
-              message = self.processMessage(message)
-              if len(message) >= 1:
-
-                  # Fire a message even if the message is unchanged
-                  if message == self.parameterNode.GetParameter("Data"):
-                    self.parameterNode.Modified()
-                  else:
-                    self.parameterNode.SetParameter("Data", message)
-
-          qt.QTimer.singleShot(1000/self.arduinoRefreshRateFps, self.pollSerialDevice)
-
-  def processMessage(self, msg):
-      return msg
 
 
 class ArduinoPedalBoardTest(ScriptedLoadableModuleTest):
